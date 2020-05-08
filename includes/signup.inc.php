7ae958c5841fb8478853ee59c758580c2925b3c8
ChibiDinos <brebell629@outlook.com>
2020-05-08 04:30:55 -0400

    Update signup.inc.php
    just adding a user to the customer table.
    
    there is a session file that should be added. it's in the second half.

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,3 +1,50 @@
 <?php
+session_start();
+if(isset($_POST['signup'])
+   {
+    $user = trim($_REQUEST('username');
+    $pass = trim($_REQUEST('password');
+
+    /* again database connections needed, assume mysql
+    
+    //can remove customer name details from the created customer table in the email
+    
+    $query = "INSERT INTO Customer VALUES ('$user', '$pass'); //the id will auto increment
+    $response = mysqli_query($dbc, $query); //where $dbc is the database connetion
+    
+    if($reponse)
+    {
+      //on a sucessful creation
+      $_SESSION['login_user] = $user;
+      header("location: home.php");
+    }
+   
+    
+    */
+                 
+                 
+    /*
+    In addition, we need a session file, that verifies the session, so make this a new file
+    
+    <?php
+   include('dbconnection.php');
+   session_start();
+   
+   $user_check = $_SESSION['login_user'];
+   
+   $ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
+   
+   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   
+   $login_session = $row['username'];
+   
+   if(!isset($_SESSION['login_user'])){
+      header("location:login.php");
+      die();
+   }
+?>
+    
+    */
+   }
 
 ?>
