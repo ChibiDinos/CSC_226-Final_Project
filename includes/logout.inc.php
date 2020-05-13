865e11a9ca86ff42b8c5b8e988e42e56aa10f2b1
Briana Pacheco <brebell629@outlook.com>
2020-05-12 20:22:04 -0400

    Update logout.inc.php
    Clears values and redirects back to the login page.

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,3 +1,11 @@
 <?php
 
+//Unset all session values.
+$_SESSION = array();
+// Destroy the session.
+session_destroy();
+ 
+//Redirect back to login page.
+header("location: login.php");
+exit;
 ?>
